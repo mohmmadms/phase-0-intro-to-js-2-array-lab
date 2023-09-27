4336a020139ea436bce4c649e4d0136d2341f321
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-09-28 00:33:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+   const copyOfCats=[...cats,"Broom"];
+   return(copyOfCats);
+}
+function prependCat(name){
+   const copyCats=["Arnold",...cats];
+   return(copyCats);
+}
+function removeLastCat(){
+   const NewCats= cats.slice(0,-1);
+   return(NewCats);
+}
+function removeFirstCat(){
+   const NewwCats= cats.slice(1);
+   return (NewwCats);
+}
